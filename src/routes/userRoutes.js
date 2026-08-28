cecf3064e4618cb0c862c17c78d9c8c14716ad60
nicole8mota-droid <nicole8mota@gmail.com>
2026-08-28 14:02:14 -0300

    Update userRoutes.js

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -10,19 +10,14 @@ const {
     deleteUser
 } = require('../controllers/userController');
 
-// GET - Listar todos os usuários
 router.get('/users', getUsers);
 
-// GET - Buscar usuário por ID
 router.get('/users/:id', getUserById);
 
-// POST - Cadastrar usuário
 router.post('/users', createUser);
 
-// PUT - Atualizar usuário
 router.put('/users/:id', updateUser);
 
-// DELETE - Remover usuário
 router.delete('/users/:id', deleteUser);
 
 module.exports = router;
