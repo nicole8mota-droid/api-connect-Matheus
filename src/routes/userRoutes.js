a8c3178f4c1abd27bbd16452ce5a77f458b51b16
nicole8mota-droid <nicole8mota@gmail.com>
2026-08-28 13:54:31 -0300

    Update userRoutes.js

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,101 +1,28 @@
 const express = require('express');
+
 const router = express.Router();
 
-const { users, generateId } = require('../data/users');
+const {
+    getUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser
+} = require('../controllers/userController');
 
 // GET - Listar todos os usuários
-router.get('/users', (req, res) => {
-    return res.status(200).json({
-        data: users
-    });
-});
+router.get('/users', getUsers);
 
 // GET - Buscar usuário por ID
-router.get('/users/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-
-    const user = users.find(user => user.id === id);
-
-    if (!user) {
-        return res.status(404).json({
-            error: 'Usuário não encontrado.'
-        });
-    }
-
-    return res.status(200).json({
-        data: user
-    });
-});
+router.get('/users/:id', getUserById);
 
 // POST - Cadastrar usuário
-router.post('/users', (req, res) => {
-    const { name, email } = req.body;
-
-    if (!name || !email) {
-        return res.status(400).json({
-            error: 'Nome e e-mail são obrigatórios.'
-        });
-    }
-
-    const newUser = {
-        id: generateId(),
-        name,
-        email
-    };
-
-    users.push(newUser);
-
-    return res.status(201).json({
-        data: newUser
-    });
-});
+router.post('/users', createUser);
 
 // PUT - Atualizar usuário
-router.put('/users/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-
-    const index = users.findIndex(user => user.id === id);
-
-    if (index === -1) {
-        return res.status(404).json({
-            error: 'Usuário não encontrado.'
-        });
-    }
-
-    const { name, email } = req.body;
-
-    if (!name || !email) {
-        return res.status(400).json({
-            error: 'Nome e e-mail são obrigatórios.'
-        });
-    }
-
-    users[index] = {
-        id,
-        name,
-        email
-    };
-
-    return res.status(200).json({
-        data: users[index]
-    });
-});
+router.put('/users/:id', updateUser);
 
 // DELETE - Remover usuário
-router.delete('/users/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-
-    const index = users.findIndex(user => user.id === id);
-
-    if (index === -1) {
-        return res.status(404).json({
-            error: 'Usuário não encontrado.'
-        });
-    }
-
-    users.splice(index, 1);
-
-    return res.status(204).send();
-});
+router.delete('/users/:id', deleteUser);
 
 module.exports = router;
