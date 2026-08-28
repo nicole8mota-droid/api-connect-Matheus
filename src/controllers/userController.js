535482e76d0b7df06301b7f3875dd8eed4827c91
nicole8mota-droid <nicole8mota@gmail.com>
2026-08-28 14:01:31 -0300

    Create userController.js

---
+++ src/controllers/userController.js
@@ -0,0 +1,96 @@
+const { users, generateId } = require('../data/users');
+
+function getUsers(req, res) {
+    return res.status(200).json({
+        data: users
+    });
+}
+
+function getUserById(req, res) {
+    const id = parseInt(req.params.id);
+    const user = users.find(user => user.id === id);
+
+    if (!user) {
+        return res.status(404).json({
+            error: 'Usuário não encontrado.'
+        });
+    }
+
+    return res.status(200).json({
+        data: user
+    });
+}
+
+function createUser(req, res) {
+    const { name, email } = req.body;
+
+    if (!name || !email) {
+        return res.status(400).json({
+            error: 'Nome e e-mail são obrigatórios.'
+        });
+    }
+
+    const newUser = {
+        id: generateId(),
+        name,
+        email
+    };
+
+    users.push(newUser);
+
+    return res.status(201).json({
+        data: newUser
+    });
+}
+
+function updateUser(req, res) {
+    const id = parseInt(req.params.id);
+    const index = users.findIndex(user => user.id === id);
+
+    if (index === -1) {
+        return res.status(404).json({
+            error: 'Usuário não encontrado.'
+        });
+    }
+
+    const { name, email } = req.body;
+
+    if (!name || !email) {
+        return res.status(400).json({
+            error: 'Nome e e-mail são obrigatórios.'
+        });
+    }
+
+    users[index] = {
+        id,
+        name,
+        email
+    };
+
+    return res.status(200).json({
+        data: users[index]
+    });
+}
+
+function deleteUser(req, res) {
+    const id = parseInt(req.params.id);
+    const index = users.findIndex(user => user.id === id);
+
+    if (index === -1) {
+        return res.status(404).json({
+            error: 'Usuário não encontrado.'
+        });
+    }
+
+    users.splice(index, 1);
+
+    return res.status(204).send();
+}
+
+module.exports = {
+    getUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser
+};
